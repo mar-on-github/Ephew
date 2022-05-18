7eae2091e4d3bbc54ae06705b62fa80bf3c08566
Mar <just.mar.ok2813@gmail.com>
2022-05-19 00:12:11 +0200

    Get started on an auto feed parser

---
+++ public/feed.rss/index.php
@@ -0,0 +1,32 @@
+<?php
+header('Content-type: application/xml');
+echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
+    <rss version=\"2.0\"
+	xmlns:content=\"http://purl.org/rss/1.0/modules/content/\"
+	xmlns:wfw=\"http://wellformedweb.org/CommentAPI/\"
+	xmlns:dc=\"http://purl.org/dc/elements/1.1/\"
+	xmlns:atom=\"http://www.w3.org/2005/Atom\"
+	xmlns:sy=\"http://purl.org/rss/1.0/modules/syndication/\"
+	xmlns:slash=\"http://purl.org/rss/1.0/modules/slash/\"
+	
+	xmlns:georss=\"http://www.georss.org/georss\"
+	xmlns:geo=\"http://www.w3.org/2003/01/geo/wgs84_pos#\">
+    <channel>\n";
+?>
+<title>Ephew</title>
+<atom:link href="http://<?php echo $_SERVER['HTTP_HOST']; ?>/feed.rss/" rel="self" type="application/rss+xml" />
+<link>http://<?php echo $_SERVER['HTTP_HOST']; ?></link>
+<description>a new aproach to social media.</description>
+<?php
+date_default_timezone_set('Europe/Amsterdam');
+$current_date = date('Y-m-d H:i:s')
+?>
+<lastBuildDate><?php echo $current_date; ?> +0000</lastBuildDate>
+<language>en-US</language>
+<sy:updatePeriod>
+    hourly </sy:updatePeriod>
+<sy:updateFrequency>
+    1 </sy:updateFrequency>
+
+</channel>
+</rss>
