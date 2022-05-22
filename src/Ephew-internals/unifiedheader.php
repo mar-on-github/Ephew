d70d13396dc0b13a4d6135d4a656d3f4125a5cc2
Mar <101558380+mar-on-github@users.noreply.github.com>
2022-05-22 02:19:04 +0200

    >:(

--- src/Ephew-internals/unifiedheader.php
+++ src/Ephew-internals/unifiedheader.php
@@ -5,7 +5,7 @@ Starting this project today.
 -->
 <?php
 require_once __DIR__ . "/../../vendor/autoload.php";
-require_once __DIR__ . "./functions.php";
+require_once __DIR__ . "/./functions.php";
 $themetype = LocateStyleSheet();
 ?>
 <!DOCTYPE html>
